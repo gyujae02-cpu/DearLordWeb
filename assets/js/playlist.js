bdef5eecba4bd728dbf94e5819d70a98b2f79e35
gyujae02-cpu <gyujae02@gmail.com>
2026-05-05 14:52:42 +0900

    Add files via upload

---
+++ assets/js/playlist.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+    
+    window.ytPlaylistData = [
+        { id: 'RzxpiSMyJ8o', title: '주님 손 잡고 일어서세요', artist: '호연하다' },
+        { id: 'BanVgeqO7m4', title: '하나님은 너를 지키시는 자', artist: '호연하다' },
+        { id: 'GU6VfynUTuA', title: '베드로의 고백', artist: '아가파오 워십' },
+        { id: 'H9ZNXLhnI7M', title: '주님 무어라 말할까요', artist: '아가파오 워십' },
+        { id: 'pZuW2CV0mXY', title: '은혜', artist: '손경민' },
+        { id: '2O_w1Am9cq0', title: '복음밖에 없습니다', artist: '손경민' },
+        { id: 'EoVAyySRJwU', title: '힘든 길 걸어온 그대여', artist: '손경민' },
+        { id: '1PldxiT4bro', title: '탕자의 고백', artist: '팀룩워십' },
+        { id: 'Qek0xGCTCIc', title: '나는 믿네', artist: '피아워십' },
+        { id: 'ZQppjVXpbbA', title: 'LOVE LETTER', artist: '제이어스' },
+        { id: '9NhESBIGvWc', title: '비 준비하시니', artist: '예람워십' }
+    ];
+})();
