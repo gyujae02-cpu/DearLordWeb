3b6d1c96c67fd7d34aa3c82b1fd3663a6c5b60b8
gyujae02-cpu <gyujae02@gmail.com>
2026-05-06 00:49:38 +0900

    Add files via upload

---
+++ playlist.js
@@ -0,0 +1,49 @@
+(function () {
+    'use strict';
+    
+    window.ytPlaylistData = [
+        { id: 'RzxpiSMyJ8o', title: '주님 손 잡고 일어서세요', artist: '호연하다' },
+        { id: 'BanVgeqO7m4', title: '하나님은 너를 지키시는 자', artist: '호연하다' },
+        { id: 'zxsPkTaN24', title: '내 이름 아시죠', artist: '호연하다' },
+
+        { id: 'GU6VfynUTuA', title: '베드로의 고백', artist: '아가파오 워십'},
+        { id: 'H9ZNXLhnI7M', title: '주님 무어라 말할까요', artist: '아가파오 워십' },
+
+        { id: '1PldxiT4bro', title: '탕자의 고백', artist: '팀룩워십 (Team Luke Worship)' },
+        { id: 'c216NU8183o', title: '내 맘을 엽니다', artist: '팀룩워십 (Team Luke Worship)' },
+        { id: 'dq3yJ350s2c', title: '나 주님을 모른다 하여도', artist: '팀룩워십 (Team Luke Worship)' },
+        { id: 'pqDTRgaY8q0', title: '고요한 밤 하늘 별들 반짝일 때', artist: '팀룩워십 (Team Luke Worship)' },
+
+        { id: 'pZuW2CV0mXY', title: '은혜', artist: '손경민 (Vocal. 지선, 이윤화, 하니, 강찬)' },
+        { id: '2O_w1Am9cq0', title: '복음밖에 없습니다', artist: '손경민 (feat.지선, 이윤화, 아이빅밴드)' },
+        { id: 'EoVAyySRJwU', title: '힘든 길 걸어온 그대여', artist: '손경민 (Feat. 이윤화, 주리)' },
+        { id: 'acS4DAMFGWA', title: '내가 매일 해야 하는 말', artist: '손경민 (With 아이빅밴드, 지선, 이윤화)' },
+
+        { id: 'Qek0xGCTCIc', title: '나는 믿네', artist: '피아워십 (F.I.A Worship)' },
+        { id: 'ovjWMKsks2Y', title: '하나님의 부르심', artist: '피아워십 (F.I.A Worship)' },
+        { id: 'KS4wNLfGD1s', title: '나는 일어섭니다', artist: '피아워십 (F.I.A Worship)' },
+        { id: '7hm8LjSGVck', title: '난 예수가 좋다오', artist: '피아워십 (F.I.A Worship)' },
+        { id: 'AmwX2-BIpRU', title: '나는 주를 섬기는 것에 후회가 없습니다', artist: '피아워십 (F.I.A Worship)' },
+
+        { id: 'ZQppjVXpbbA', title: 'LOVE LETTER', artist: '제이어스 (J-US)' },
+        { id: 'lToefu_xLQI', title: '예수 우리 왕이여', artist: '제이어스 (J-US)' },
+        { id: 'NYiH9ftHjWo', title: '여호와께 돌아가자', artist: '제이어스 (J-US)' },
+        { id: 'MMzUCCn6aQ4', title: '나 같은 죄인 살리신', artist: '제이어스 (J-US)' },
+
+        { id: '8PEKtpFsWiI', title: '주가 일하시네', artist: '예람워십 (Yeram Worship)' },
+        { id: 'pjoTQN7Sy1I', title: '오직 주만이 / 주의 음성을 내가 들으니', artist: '예람워십 (Yeram Worship)' },
+
+        { id: '3ZFnSEH6Hfk', title: '나의 안에 거하라', artist: '달빛마을 (Moonlight Village)' },
+        { id: 'EDvF_eGRt48', title: '나 주를 사랑합니다', artist: '달빛마을 (Moonlight Village)' },
+
+        { id: 't0CYXeqYdnE', title: '꽃들도 (花も)', artist: '제이워십' },
+        { id: 'CDeWAlagzQc', title: '주님을 바라봅니다', artist: '위클레시아 (WECCLESIA)' },
+        { id: 'ctP6td3vTpo', title: '주 예수 내 산 소망', artist: '데이빗밴드 (Daybeat Band)' },
+        { id: 'Wxp2h2a9P34', title: '아무것도 두려워 말라', artist: 'Gina' },
+        { id: 'Ohyi6WsdbVQ', title: 'The Lord\'s Prayer', artist: '브리지임팩트 프레이즈' },
+        { id: '95vtJk3nEOQ', title: '비 준비하시니 Psalm 147', artist: '심형진 | Stonegate Music' },
+        { id: 'zZLNzOMIZKY', title: '기대 (주 안에 우린 하나)', artist: 'Luvim' },
+        { id: '9vS0aGxVHjk', title: '전능하신 나의 주 하나님은', artist: '마커스워십 (Markers Worship)' },
+        { id: '_OJLMWtkYWE', title: '깊은 곳에 나아가 / 주 품에', artist: 'WELOVE' },
+    ];
+})();
