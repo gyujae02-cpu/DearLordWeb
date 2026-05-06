dceb85931421f7cc0b11762cadfc461857efb8a8
gyujae02-cpu <gyujae02@gmail.com>
2026-05-06 23:59:48 +0900

    Add files via upload

--- assets/js/playlist.js
+++ assets/js/playlist.js
@@ -1,11 +1,68 @@
 (function () {
     'use strict';
-    
+
     window.ytPlaylistData = [
         { id: 'RzxpiSMyJ8o', title: '주님 손 잡고 일어서세요', artist: '호연하다' },
         { id: 'BanVgeqO7m4', title: '하나님은 너를 지키시는 자', artist: '호연하다' },
 
-        { id: 'GU6VfynUTuA', title: '베드로의 고백', artist: '아가파오 워십'},
+        {
+            id: 'GU6VfynUTuA', title: '베드로의 고백', artist: '아가파오 워십',
+            lyrics:
+                [
+                    { start: 0, text: "내가 가장 후회하지 않는 일" },
+                    { start: 26, text: "예수를 따르는 일" },
+                    { start: 34.5, text: "내 그물 버리고, 내 사랑들 뒤로 하고" },
+                    { start: 42, text: "나를 부르신 주님과 동행하는 일" },
+                    { start: 50, text: "나의 평생에 소원하는 일" },
+                    { start: 57.5, text: "예수를 닮아 가는 일" },
+                    { start: 66, text: "굳은 마음 제하시고, 사랑을 가르치신" },
+                    { start: 73, text: "나의 예수님을 사랑하는 일" },
+                    { start: 80, text: "날 부르심에 감사, 날 놓지 않으심 감사" },
+                    { start: 88, text: "하늘 보좌 버리고 날 사랑하심에 감사" },
+                    { start: 97, text: "나의 모든 삶을, 남은 모든 인생을" },
+                    { start: 104, text: "예수님과 함께 걷겠습니다" },
+                    { start: 116, text: "내가 가장 후회하지 않는 일" },
+                    { start: 123, text: "예수를 따르는 일" },
+                    { start: 132, text: "내 그물 버리고, 내 사랑들 뒤로 하고" },
+                    { start: 138.5, text: "나를 부르신 주님과 동행하는 일" },
+                    { start: 147, text: "나의 평생에 소원하는 일" },
+                    { start: 154, text: "예수를 닮아 가는 일" },
+                    { start: 162, text: "굳은 마음 제하시고, 사랑을 가르치신" },
+                    { start: 169, text: "나의 예수님을 사랑하는 일" },
+                    { start: 176, text: "날 부르심에 감사, 날 놓지 않으심 감사" },
+                    { start: 185, text: "하늘 보좌 버리고 날 사랑하심에 감사" },
+                    { start: 193, text: "나의 모든 삶을, 남은 모든 인생을" },
+                    { start: 200, text: "예수님과 함께 걷겠습니다" },
+                    { start: 207, text: "날 부르심에 감사, 날 놓지 않으심 감사" },
+                    { start: 215, text: "하늘 보좌 버리고 날 사랑하심에 감사" },
+                    { start: 223, text: "나의 모든 삶을, 남은 모든 인생을" },
+                    { start: 230.5, text: "예수님과 함께 걷겠습니다" },
+                    { start: 266, text: "니가 나를 사랑하느냐?" },
+                    { start: 274, text: "니가 나를 사랑하느냐?" },
+                    { start: 281, text: "니가 나를 사랑하느냐?" },
+                    { start: 289, text: "주님 내게 물어보시네" },
+                    { start: 297, text: "내가 주를 사랑합니다" },
+                    { start: 304, text: "내가 주를 사랑합니다" },
+                    { start: 312, text: "내가 주를 사랑합니다" },
+                    { start: 320, text: "내가 주를 사랑합니다" },
+                    { start: 327, text: "내가 주를 사랑합니다" },
+                    { start: 335, text: "내가 주를 사랑합니다" },
+                    { start: 343, text: "내가 주를 사랑합니다" },
+                    { start: 350, text: "내 사랑을 주가 아십니다" },
+                    { start: 357, text: "날 부르심에 감사, 날 놓지 않으심 감사" },
+                    { start: 365, text: "하늘 보좌 버리고 날 사랑하심에 감사" },
+                    { start: 373, text: "나의 모든 삶을, 남은 모든 인생을" },//
+                    { start: 380, text: "예수님과 함께 걷겠습니다" },//
+                    { start: 388, text: "날 부르심에 감사, 날 놓지 않으심 감사" },
+                    { start: 396, text: "하늘 보좌 버리고 날 사랑하심에 감사" },
+                    { start: 404, text: "나의 모든 삶을, 남은 모든 인생을" },//
+                    { start: 411, text: "예수님과 함께 걷겠습니다" },
+                    { start: 420, text: "나의 모든 삶을" },
+                    { start: 424, text: "남은 모든 인생을" },
+                    { start: 427, text: "예수님과 함께" },
+                    { start: 430.5, text: "걷겠습니다" }
+                ]
+        },
         { id: 'H9ZNXLhnI7M', title: '주님 무어라 말할까요', artist: '아가파오 워십' },
 
         { id: '1PldxiT4bro', title: '탕자의 고백', artist: '팀룩워십 (Team Luke Worship)' },
@@ -32,8 +89,10 @@
         { id: '8PEKtpFsWiI', title: '주가 일하시네', artist: '예람워십 (Yeram Worship)' },
         { id: 'pjoTQN7Sy1I', title: '오직 주만이 / 주의 음성을 내가 들으니', artist: '예람워십 (Yeram Worship)' },
 
-        { id: 't0CYXeqYdnE', title: '꽃들도 (花も)', artist: '제이워십' },
         { id: '3ZFnSEH6Hfk', title: '나의 안에 거하라', artist: '달빛마을 (Moonlight Village)' },
+        { id: 'EDvF_eGRt48', title: '나 주를 사랑합니다', artist: '달빛마을 (Moonlight Village)' },
+
+        { id: 't0CYXeqYdnE', title: '꽃들도 (花も)', artist: '제이워십' },        
         { id: 'CDeWAlagzQc', title: '주님을 바라봅니다', artist: '위클레시아 (WECCLESIA)' },
         { id: 'ctP6td3vTpo', title: '주 예수 내 산 소망', artist: '데이빗밴드 (Daybeat Band)' },
         { id: 'Wxp2h2a9P34', title: '아무것도 두려워 말라', artist: 'Gina' },
@@ -42,6 +101,5 @@
         { id: 'zZLNzOMIZKY', title: '기대 (주 안에 우린 하나)', artist: 'Luvim' },
         { id: '9vS0aGxVHjk', title: '전능하신 나의 주 하나님은', artist: '마커스워십 (Markers Worship)' },
         { id: '_OJLMWtkYWE', title: '깊은 곳에 나아가 / 주 품에', artist: 'WELOVE' },
-
     ];
 })();
